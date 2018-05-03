e3caabbdcea1296e054d21cd4a5bd6861f80d478
Michael Olkin <34135006+MikeOlkinSwsc@users.noreply.github.com>
2018-05-03 10:46:58 -0400

    Delete STIntersectionAreaExample.sql

--- STIntersectionAreaExample.sql
+++
@@ -1,7 +0,0 @@
- DECLARE @g geometry = 'CURVEPOLYGON (CIRCULARSTRING (0 -4, 4 0, 0 4, -4 0, 0 -4))';  
- DECLARE @h geometry = 'POLYGON ((1 -1, 5 -1, 5 3, 1 3, 1 -1))';  
- SELECT format( @h.STIntersection(@g).STArea() / @h.STArea(), 'P1') as IntersectArea
-		,@h.STIntersection(@g) as Shape
-		,@g as Circle
-		,@h as Polygon
-
